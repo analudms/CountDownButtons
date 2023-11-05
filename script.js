c2b3d04dd91dee20f5ec20c577767ef3a09ce885
analudms <analuizamelo3m@gmail.com>
2023-11-05 00:39:00 -0300

    adding comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//pegando el no html
 const buttonStart = document.querySelector('#buttonStart');
 const buttonStop = document.querySelector('#buttonStop');
 const buttonReset = document.querySelector('#buttonReset');
@@ -12,38 +13,43 @@ let seconds = 0;
 let milliseconds = 0;
 let isPaused = false;
 
+// adicionando eventos
 buttonStart.addEventListener("click", startTimer)
 buttonStop.addEventListener("click", pauseTimer)
 buttonResume.addEventListener("click", resumeTimer)
 buttonReset.addEventListener("click", resetTimer)
 
+//função que inicia 
 function startTimer() {
+    //definindo um intervalo que executará uma função a cada 10 segundos 
     interval = setInterval(() => {
+        //se for verdadeira não executa
         if (!isPaused){
+            //incrementa em 10 a cada iteração.
             milliseconds += 10;
 
+            //verifica se a variável atingiu 1000, se sim ela adiciona mais um segundo à variável seconds
             if(milliseconds === 1000){
                 seconds++;
                 milliseconds = 0;
             }
 
+            //verifica se a variável atingiu 60, se sim ela adiciona mais um minuto à variável minutes
             if(seconds === 60){
                 minutes++;
                 seconds = 0;
             }
-
+            //mudança do conteúdo do html
             minutesEl.textContent = formatTime(minutes);
             secondsEl.textContent = formatTime(seconds);
             millisecondsEl.textContent = formatMilliseconds(milliseconds);
-
         }
     },10)
 }
-  
+//se o time for < que 10 ele add um 0 à frente
 function formatTime(time) {
     return time < 10 ? `0${time}` : time
 }
-
 function formatMilliseconds (time) {
     return time < 100 ? `${time}`.padStart(3, "0") : time
 }
@@ -67,7 +73,4 @@ function resetTimer(){
     millisecondsEl.textContent = "000";
 
     isPaused = false; 
-
-
-
 }
