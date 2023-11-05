f457c4ef9eed3904f365d981d634b07363992b92
analudms <analuizamelo3m@gmail.com>
2023-11-04 23:13:40 -0300

    creating functions to cronometer

---
+++ script.js
@@ -0,0 +1,57 @@
+const buttonStart = document.querySelector('#buttonStart');
+const buttonStop = document.querySelector('#buttonStop');
+const buttonReset = document.querySelector('#buttonReset');
+
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+const millisecondsEl = document.querySelector('#milliseconds ');
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let isPaused = false;
+
+buttonStart.addEventListener("click", startTimer)
+buttonStop.addEventListener("click", pauseTimer)
+buttonResume.addEventListener("click", resumeTimer)
+
+function startTimer() {
+    interval = setInterval(() => {
+        if (!isPaused){
+            milliseconds += 10;
+
+            if(milliseconds === 1000){
+                seconds++;
+                milliseconds = 0;
+            }
+
+            if(seconds === 60){
+                minutes++;
+                seconds = 0;
+            }
+
+            minutesEl.textContent = formatTime(minutes);
+            secondsEl.textContent = formatTime(seconds);
+            millisecondsEl.textContent = formatMilliseconds(milliseconds);
+
+        }
+    },10)
+}
+  
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+function formatMilliseconds (time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time
+}
+
+function pauseTimer(){
+    isPaused = true;
+}
+
+function resumeTimer(){
+    isPaused = false;
+}
+
